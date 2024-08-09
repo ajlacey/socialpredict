8aba2c447238b90b64620d72d6477ff1319e20a3
Arthur Lacey <25300182+ajlacey@users.noreply.github.com>
2024-08-08 23:07:44 -0400

    add unit tests for valid and invalid description lengths

--- backend/handlers/markets/createmarket_test.go
+++ backend/handlers/markets/createmarket_test.go
@@ -28,6 +28,7 @@ func TestCheckQuestionTitleLength_invalid(t *testing.T) {
 	}
 }
 
+// TODO: Add test description
 func TestCheckQuestionTitleLength_valid(t *testing.T) {
 	tests := []struct {
 		testname      string
@@ -48,4 +49,44 @@ func TestCheckQuestionTitleLength_valid(t *testing.T) {
 			t.Errorf("Unexpected error in test %s", test.testname)
 		}
 	}
+}
+
+// TestCheckQuestionDescriptionLength_invalid ensures an error is returned if
+// the question exceeds the maxDescriptionLength
+func TestCheckQuestionDescriptionLength_invalid(t *testing.T) {
+	test := struct {
+		testname    string
+		description string
+	}{
+		testname:    "One character too many",
+		description: strings.Repeat("a", maxDescriptionLength+1),
+	}
+
+	if err := checkQuestionDescriptionLength(test.description); err == nil {
+		t.Errorf("Expected error in test %s, got nil", test.testname)
+	}
+}
+
+// TestCheckQuestionDescriptionLength_valid checks the upper and lower bounds
+// of the permissible description length, ensuring no errors are returned.
+func TestCheckQuestionDescriptionLength_valid(t *testing.T) {
+	tests := []struct {
+		testname    string
+		description string
+	}{
+		{
+			testname:    "Maximum length",
+			description: strings.Repeat("a", maxDescriptionLength),
+		},
+		{
+			testname:    "Empty description",
+			description: "",
+		},
+	}
+	for _, test := range tests {
+		if err := checkQuestionDescriptionLength(test.description); err != nil {
+			t.Errorf("Unexpected error in test %s, got nil", test.testname)
+		}
+	}
+
 }
